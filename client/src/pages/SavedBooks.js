d8ada7eff79585594c0dc5909cf4e6a22bc0bfc0
Michael Salisbury <m.slzbry@gmail.com>
2023-09-05 17:45:02 -0700

    Refactor signup form

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -7,8 +7,6 @@ import {
   Col
 } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
-import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_ME } from "../utils/queries";
